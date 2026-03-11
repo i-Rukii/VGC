7ec405c8dc5306d53934c121e05ea73b12f3f1c9
Gris <lazyzhouu@gmail.com>
2026-03-11 05:08:17 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   },
 };
 
